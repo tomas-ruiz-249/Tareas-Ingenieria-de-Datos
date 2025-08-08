a46fb5747bb2fa252cc264bd927d218bf1250663
Prestamo Estudiantes <prestamour@urosario.edu.co>
2025-08-08 09:02:47 -0500

    clase

---
+++ ETL/ETL en clase/etl.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlite3
+from pathlib import Path
+
+FILE_PATH = 'data.csv'
+OUTPUT_DB = 'data.db'
+
+TABLE_NAME = 'ventas'
+
+def clasificarViajes():
+    pass
+
+def main():
+    
