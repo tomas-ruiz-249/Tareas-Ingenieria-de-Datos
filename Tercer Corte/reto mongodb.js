1dadf33a6255b192256edb4cf7fc443c33ae38bb
tomas <tomas.ruizcorrea@gmail.com>
2025-11-05 08:51:58 -0500

    retos mongodb

---
+++ Tercer Corte/reto mongodb.js
@@ -0,0 +1,88 @@
+//RETO 1
+db.bands.insertMany([
+  {
+    name: 'Soda Stereo',
+    year: 1982,
+    country: 'Argentina',
+    albums: [
+      {
+        name: 'Nada Personal',
+        songs: [ 'Cuando Pase el Temblor', 'Imagenes Retro' ],
+        year: 1985,
+        rating: 4.5,
+        sales: 150000
+      },
+      {
+        name: 'Signos',
+        songs: [ 'Persiana Americana', 'El Rito' ],
+        year: 1986,
+        rating: 4.8,
+        sales: 250000
+      }
+    ],
+    members: [ 'Gustavo Cerati', 'Zeta Bosio', 'Charly Alberti' ]
+  },
+  {
+    name: 'Seru Giran',
+    year: 1978,
+    country: 'Argentina',
+    albums: [
+      {
+        name: 'Peperina',
+        songs: [ 'Esperando Nacer', 'Cinema Verite' ],
+        year: 1981,
+        rating: 4.6,
+        sales: 350000
+      },
+      {
+        name: 'La Grasa de las Capitales',
+        songs: [ 'Paranoia y Soledad', 'Frecuencia Modulada' ],
+        year: 1971,
+        rating: 4.8,
+        sales: 550000
+      }
+    ]
+  },
+  {
+    name: 'The Beatles',
+    year: 1960,
+    country: 'England',
+    albums: [
+      {
+        name: 'Abbey Road',
+        songs: [ 'Come Together', 'Something' ],
+        rating: 5,
+        sales: 600000
+      },
+      {
+        name: 'Let it Be',
+        songs: [ 'Across the Universe', "I've Got A Feeling" ],
+        rating: 4.3,
+        sales: 450000
+      }
+    ]
+  }
+])
+
+//RETO 2
+db.bands.aggregate([
+    {$project : 
+        {"name" : 1, "numAlbums" : {$size : "$albums"}}
+    }
+])
+
+//RETO 3
+db.bands.aggregate([
+    { $unwind: "$albums" },
+    { $project: { "name": 1, "albumName": "$albums.name", "sales": "$albums.sales" } },
+    { $sort: { "sales": -1 } },
+    {$limit: 2}
+])
+
+//RETO 4
+db.bands.aggregate([
+    { $unwind: "$albums" },
+    { $project: { "name": 1, "albumName": "$albums.name", "rating": "$albums.rating" } },
+    { $sort: { "rating": -1 } },
+    {$limit: 2}
+])
